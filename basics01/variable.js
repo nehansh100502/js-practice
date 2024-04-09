009247d8d7a10be4ebb4bd1cdbe4ba23588a146e
nehansh100502 <155417820+nehansh100502@users.noreply.github.com>
2024-04-09 12:53:37 +0000

    learn variable

---
+++ basics01/variable.js
@@ -0,0 +1,17 @@
+const accountId = 122223
+let emailId = "neha123@gmail.com"
+var  password = "12345"
+accountCity = "prayagraj"
+let height ;
+
+emailId ="hema3212gmail.com "
+password ="123455"
+accountCity="kanpur"
+
+console.log(accountId)
+console.log([accountId,emailId,password,accountCity,height])
+
+/*
+do not use var 
+because of issue in block scope and functional scope 
+*/
